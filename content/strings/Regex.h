ea53caa214e636e65b07eb4b18baaa496cdb7f6e
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-09-29 02:20:40 +0200

    Add Regex.h

---
+++ content/strings/Regex.h
@@ -0,0 +1,19 @@
+/**
+ * Author: Asger Hautop Drewsen
+ * Date: 2017-09-29
+ * License: CC0
+ * Description: Negative return value means no match found.
+ * Time: O(N M)
+ */
+#pragma once
+
+#include <regex.h> /** keep-include */
+
+string pattern = "a*";
+regex_t re{};
+re.fastmap = (char *)malloc(256);
+re_compile_pattern(pattern.data(), sz(pattern), &re);
+string S = "aaa";
+int matched = re_match(&buf, S.data(), sz(S), 0, nullptr);
+int i = re_search(&buf, S.data(), sz(S), 0, sz(S), nullptr);
+regfree(&re);
