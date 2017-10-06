a034663474e0734bbf297107dc6bce30c6170bc5
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-10-06 18:32:14 +0200

    Add endl macro to c++ template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define trav(a, x) for(auto& a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
+#define endl "\n"
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
