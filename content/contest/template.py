5e784d4974f7a197fa644f8e84fb70d47af50418
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-10-26 03:04:24 +0200

    Update bashrc, vimrc and template.py

--- content/contest/template.py
+++ content/contest/template.py
@@ -1,7 +1,4 @@
 from __future__ import division, print_function
-try:
-	input = raw_input
-	range = xrange
-	from itertools import izip as zip, imap as map, ifilter as filter
-except NameError:
-	pass
+input = raw_input
+range = xrange
+from itertools import izip as zip, imap as map, ifilter as filter
