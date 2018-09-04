6561cbdfeadc969b6aa7590ff9b3a0b4de084eb6
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-09-05 00:27:49 +0200

    Minor changes to template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define trav(a, x) for (auto & a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
 #define endl "\n"
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
 
 int main() {
 	cin.sync_with_stdio(0); cin.tie(0);
