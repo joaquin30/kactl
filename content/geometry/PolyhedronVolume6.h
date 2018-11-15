b5ed61eacbb57220f3282af9af439ca594effc6c
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-11-15 02:15:15 +0100

    Add PolyhedronVolume6.h

---
+++ content/geometry/PolyhedronVolume6.h
@@ -0,0 +1,34 @@
+/**
+ * Author: Asger Hautop Drewsen
+ * Date: 2018-11-15
+ * License: CC0
+ * Source: https://wwwf.imperial.ac.uk/~rn/centroid.pdf
+ * Description: Returns six times the volume of a polyhedron.
+ *  Points in faces can be given in either cw or ccw order.
+ * Status: Tested with Kattis problem threedprinter
+ */
+#pragma once
+
+#include "Point3D.h"
+
+template<class T>
+T polyhedronVolume6(const vector<vector<Point3D<T>>> & faces) {
+	T volume = 0;
+	Point3D<T> center;
+	set<Point3D<T>> vertices;
+	trav(face, faces) trav(p, face) {
+		if (!vertices.count(p)) {
+			center = center + p;
+			vertices.insert(p);
+		}
+	}
+	center = center / sz(vertices);
+	trav(f, faces) {
+		rep(i, 1, sz(f) - 1) {
+			auto normal = (f[i] - f[0]).cross(f[i+1] - f[0]);
+			T s = normal.dot(center - f[0]) < 0? 1: -1;
+			volume += f[0].dot(normal) * s;
+		}
+	}
+	return volume;
+}
