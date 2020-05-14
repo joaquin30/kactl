6776b4edc6bf1f2683a07dc0f57e0c0454eb5b4b
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-05-14 12:06:29 +0200

    Make TSP more general

--- content/graph/TSP.h
+++ content/graph/TSP.h
@@ -2,27 +2,26 @@
  * Author: Oskar Haarklou Veileborg
  * Date: 2020-04-14
  * License: CC0
- * Description: Given a distance matrix, computes the min. length of
- * a tour/path beginning (and ending) at node N visiting all other nodes exactly once.
- * Time: O(2^n n^2). For n=22 $\approx 0.5s$.
+ * Description: Given a distance matrix, computes for all $i < S = N - 1$,
+ * the min.\ length of all paths visiting all nodes starting at $S$ and ending at $i$.
+ * To compute the cyclic TSP-tour length use $\min \{ \mathrm{res}[i] + C[i][S] \mid i < S \}$.
+ * Time: O(2^N N^2). $N = 22 \approx 0.5 s$.
  * Status: Stress-tested and tested on Kattis: toursdesalesforce, supermario169
  */
 #pragma once
 
-const int MAXN = 21; // 1 less than actual MAXN is fine
+const int MAXS = 21; // 1 less than MAXN
 using T = ll; // can also use double
 const T INF = 1e12;
 #define repbits(x, bs) for(int x, y = bs; \
   y && ((x = __builtin_ctz(y)) || y); y ^= y&-y)
-T DP[1 << MAXN][MAXN];
-T TSP(const vector<vector<T>>& C) {
-  int N = sz(C)-1, M = 1 << N;
-  rep(i, 0, N) DP[1 << i][i] = C[N][i];
+T DP[1 << MAXS][MAXS];
+T * TSP(const vector<vector<T>>& C) {
+  int S = sz(C)-1, M = 1 << S;
+  rep(i, 0, S) DP[1 << i][i] = C[S][i];
   rep(bs, 3, M) if(__builtin_popcount(bs) > 1) repbits(j, bs) {
     int nbs = bs ^ (1 << j); T& r = DP[bs][j] = INF;
     repbits(i, nbs) r = min(r, DP[nbs][i] + C[i][j]);
   }
-  T best = INF; // remove (+ C[i][N]) to get best path
-  rep(i, 0, N) best = min(best, DP[M-1][i] + C[i][N]);
-  return best;  // ending at i
+  return DP[M-1];
 }
--- stress-tests/graph/TSP.cpp
+++ stress-tests/graph/TSP.cpp
@@ -17,22 +17,17 @@ vector<vd> make_cost(const vector<P>& ps) {
 }
 
 namespace TSPD {
-    const int MAXN = 21; // 1 less than actual MAXN is fine
     using T = double; // can also use double
     const T INF = 1e12;
-    T DP[1 << MAXN][MAXN];
-    T TSP(const vector<vector<T>>& C) {
-      int N = sz(C)-1, M = 1 << N;
-      rep(bs, 1, M) rep(i, 0, N) DP[bs][i] = INF;
-      rep(i, 0, N) DP[1 << i][i] = C[i][N];
-      rep(bs, 1, M) repbits(j, bs) {
-        int nbs = bs ^ (1 << j);
-        repbits(i, nbs)
-          DP[bs][j] = min(DP[bs][j], DP[nbs][i] + C[i][j]);
+    T DP[1 << MAXS][MAXS];
+    T * TSP(const vector<vector<T>>& C) {
+      int S = sz(C)-1, M = 1 << S;
+      rep(i, 0, S) DP[1 << i][i] = C[S][i];
+      rep(bs, 3, M) if(__builtin_popcount(bs) > 1) repbits(j, bs) {
+        int nbs = bs ^ (1 << j); T& r = DP[bs][j] = INF;
+        repbits(i, nbs) r = min(r, DP[nbs][i] + C[i][j]);
       }
-      T best = INF;
-      rep(i, 0, N) best = min(best, DP[M-1][i] + C[i][N]);
-      return best;
+      return DP[M-1];
     }
 }
 
@@ -70,20 +65,60 @@ template<class T> T perm(const vector<vt>& C) {
     return best;
 }
 
+template<class T> vt perm_path(const vector<vt>& C, int S) {
+    assert(S == sz(C) - 1); // for simplicity of implementation
+    vi order(sz(C) - 1);
+    vt best(sz(C) - 1, INF);
+    iota(all(order), 0);
+    do {
+        int prev = S;
+        T cost = 0;
+        rep(i, 0, sz(C) - 1) {
+            cost += C[prev][order[i]];
+            prev = order[i];
+        }
+        int last = order.back();
+        best[last] = min(best[last], cost);
+    } while(next_permutation(all(order)));
+    return best;
+}
+
+template <typename T, typename F>
+T min_tour(F tsp_f, const vector<vector<T>> & cost) {
+    auto res = tsp_f(cost);
+    T best = INF;
+    int S = sz(cost) - 1;
+    rep(i, 0, S) best = min(best, res[i] + cost[i][S]);
+    return best;
+}
+
 using vl = vector<ll>;
 vector<vl> matrix(int N) {
     vector<vl> cost(N, vl(N));
-    rep(i, 0, N) rep(j, i + 1, N) cost[i][j] = cost[j][i] = randRange((int64_t)INF/N);
+    rep(i, 0, N) rep(j, 0, N) {
+        if (i == j) continue;
+        cost[i][j] = randRange((int64_t)INF/N);
+    }
     return cost;
 }
 
 void test_correctness(int iters) {
     rep(_, 0, iters) {
         int N = randIncl(2, 10);
-        // generate symmetric distance matrix
+        // generate asymmetric distance matrix
         auto cost = matrix(N);
-        auto c1 = TSP(cost), c2 = perm(cost);
+        auto c1 = min_tour(TSP, cost), c2 = perm(cost);
         assert(c1 == c2);
+    }
+
+    rep(_, 0, iters) {
+        int N = randIncl(2, 10);
+        auto cost = matrix(N);
+        auto r1 = TSP(cost);
+        auto r2 = perm_path(cost, N - 1);
+        rep(i, 0, N - 1) {
+            assert(r1[i] == r2[i]);
+        }
     }
 
     int64_t lim = 1 << 20;
@@ -92,7 +127,7 @@ void test_correctness(int iters) {
         vector<P> ps(N);
         trav(p, ps) p.x = randIncl(-lim, lim), p.y = randIncl(-lim, lim);
         auto cost = make_cost(ps);
-        assert(fabs(TSPD::TSP(cost) - perm(cost)) < 1e-7);
+        assert(fabs(min_tour(TSPD::TSP, cost) - perm(cost)) < 1e-7);
     }
 }
 
@@ -102,7 +137,7 @@ void test_time() {
         ll t1, t2;
         {
             timeit t("TSP: N = " + to_string(N));
-            t1 = TSP(cost);
+            t1 = min_tour(TSP, cost);
         }
         {
             timeit t("NDP: N = " + to_string(N));
