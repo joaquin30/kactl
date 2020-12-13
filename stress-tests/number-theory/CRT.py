829794fee223561d80d0d5e89e1925379a46314b
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-12-13 16:47:22 +0100

    Add CRT.py stress-test

---
+++ stress-tests/number-theory/CRT.py
@@ -0,0 +1,28 @@
+from math import lcm
+from pathlib import Path
+
+import hypothesis.strategies as st
+from hypothesis import given, settings
+
+kactl_path, crt_path = __file__.rsplit("stress-tests/", 1)
+
+with (Path(kactl_path) / "content" / crt_path).open() as f:
+    exec(f.read())
+
+
+@st.composite
+def crt_input(draw):
+    mod = draw(st.lists(st.integers(min_value=1)))
+    N = lcm(*mod)
+    x = draw(st.integers(min_value=0, max_value=N - 1))
+    rem = [x % m for m in mod]
+    return (rem, mod, x)
+
+
+@settings(max_examples=10**4)
+@given(crt_input())
+def test_crt(args):
+    rem, mod, x = args
+
+    x2 = crt(rem, mod)
+    assert x == x2
