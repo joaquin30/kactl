2912dbc97334c9a271302c492bf41b435a4d27f3
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2021-03-19 11:23:09 +0100

    Make description of InverseSegmentTree more detailed

--- content/data-structures/InverseSegmentTree.h
+++ content/data-structures/InverseSegmentTree.h
@@ -5,16 +5,19 @@
  * Source: folklore
  * Description: Zero-indexed max-tree with range updates and point queries.
  * Bounds are inclusive to the left and exclusive to the right. Can be changed by modifying T, f and unit.
+ * More limited than LazySegmentTree but much better constant factors.
+ * Update performs: $a[i] = f(a[i], val), i \in [b,e)$ where $a$ is the underlying array.
  * Time: O(\log N)
  * Status: stress-tested
- * Details: Not sure if non-commutative functions even make sense for this kind of tree?
+ * Details: See stress tests for applications.
  */
 #pragma once
 
 struct Tree {
 	typedef int T;
 	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative, commutative fn)
+	// (any associative, commutative fn)
+	T f(T a, T b) { return max(a, b); }
 	vector<T> s; int n;
 	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
 	T query(int pos) {
@@ -25,7 +28,7 @@ struct Tree {
 	void update(int b, int e, T val) { // update [b, e)
 		for (b += n, e += n; b < e; b /= 2, e /= 2) {
 			if (b % 2) s[b] = f(val, s[b]), b++;
-			if (e % 2) --e, s[e] = f(s[e], val);
+			if (e % 2) --e, s[e] = f(val, s[e]);
 		}
 	}
 };
--- stress-tests/data-structures/InverseSegmentTree.cpp
+++ stress-tests/data-structures/InverseSegmentTree.cpp
@@ -19,10 +19,35 @@ namespace sum {
 	  void update(int b, int e, T val) { // update [b, e)
 		  for (b += n, e += n; b < e; b /= 2, e /= 2) {
 			  if (b % 2) s[b] = f(val, s[b]), b++;
-			  if (e % 2) --e, s[e] = f(s[e], val);
+			  if (e % 2) --e, s[e] = f(val, s[e]);
 		  }
 	  }
   };
+}
+
+namespace assign {
+	// Assignment on ranges by using a max-tree on timestamps
+	struct Tree {
+		typedef int T;
+		static constexpr T unit = 0;
+		// (any associative, commutative fn)
+		T f(T a, T b) { return max(a, b); }
+		vector<T> s, vals; int n;
+		Tree(int n = 0, T def = unit) : s(2*n, def), vals(1, 0), n(n) {}
+		T query(int pos) {
+		  T res = s[pos += n];
+		  while(pos /= 2) res = f(res, s[pos]);
+		  return vals[res];
+		}
+		void update(int b, int e, T val) { // update [b, e)
+			int ts = sz(vals);
+			vals.push_back(val);
+			for (b += n, e += n; b < e; b /= 2, e /= 2) {
+				if (b % 2) s[b] = f(ts, s[b]), b++;
+				if (e % 2) --e, s[e] = f(ts, s[e]);
+			}
+		}
+	};
 }
 
 
@@ -50,9 +75,9 @@ int main() {
 
 	// Stress maximum
 	for(int n = 5; n < 100; n += 5) {
-	  maximum::Tree tr(n);
+		maximum::Tree tr(n);
 		vi v(n, tr.unit);
-		rep(it,0,1000000) {
+		rep(it,0,100'000) {
 			int i = rand() % n, j = rand() % (n+1);
 			int x = rand() % (n+2);
 
@@ -71,7 +96,7 @@ int main() {
 	for(int n = 5; n < 100; n += 5) {
 		sum::Tree tr(n);
 		vi v(n, tr.unit);
-		rep(it,0,1000000) {
+		rep(it,0,1'000'000) {
 			int i = rand() % n, j = rand() % (n+1);
 
 			int r = rand() % 100;
@@ -81,6 +106,25 @@ int main() {
 			else {
 				int x = rand() % (2*n+1) - n;
 				rep(k,i,j) v[k] += x;
+				tr.update(i, j, x);
+			}
+		}
+	}
+
+	// Stress assign
+	for(int n = 5; n < 100; n += 5) {
+		assign::Tree tr(n);
+		vi v(n, 0);
+		rep(it,0,1'000'000) {
+			int i = rand() % n, j = rand() % (n+1);
+			int x = rand();
+
+			int r = rand() % 100;
+			if (r < 30) {
+				assert(v[i] == tr.query(i));
+			}
+			else {
+				rep(k,i,j) v[k] = x;
 				tr.update(i, j, x);
 			}
 		}
