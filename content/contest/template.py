752d9ee2608120c47b2a901c794615d9cc961c4d
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2021-03-27 22:55:27 +0100

    Add sys.setrecursionlimit to template.py

--- content/contest/template.py
+++ content/contest/template.py
@@ -4,6 +4,7 @@
 	Returns an empty string instead of raising EOFError!
 '''
 import sys
+sys.setrecursionlimit(10**9)
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 # Many calls to print is also slow, consider:
 print('\n'.join(buffered_output))
