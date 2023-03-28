0dc26da14b9cde94c84e742effd15768d76eb6f6
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2023-03-28 16:01:14 +0200

    Describe how to construct a block-cut tree

---
+++ content/graph/BlockCutTree.h
@@ -0,0 +1,10 @@
+/**
+ * Author: Oskar Haarklou Veileborg
+ * Date: 2023-03-28
+ * Source: Wikipedia
+ * Description: A connected graph has a tree decomposition based on biconnected
+ * components. Block-nodes represent groups of nodes and are formed from the
+ * nodes in each component \emph{and} for each pair of nodes connected by a
+ * bridge. One AP-node is created for each articulation point. Add edges from
+ * AP-nodes to each block the AP is in.
+ */
