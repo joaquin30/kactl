08614665520fc258486675d638eaaea463f8de51
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2025-10-03 19:14:52 -0500

    centroid decomp modifications

--- content/graph/CentroidDecomposition.h
+++ content/graph/CentroidDecomposition.h
@@ -10,30 +10,30 @@
  * Status: Tested at CodeForces
  */
 
-const int MAXN = 2e5+10;
+const int maxn = 2e5+10;
 vector<vi> adj;
-bool del[MAXN];
-int par[MAXN]; // parent in centroid decomposition
-int szt[MAXN]; // size of subtree
+bool del[maxn];
+int par[maxn]; // parent in centroid decomposition
+int szt[maxn]; // size of subtree
 int calcsz(int u, int p) {
-    szt[u] = 1;
-    for (int v : adj[u]) if (v != p && !del[v])
-        szt[u] += calcsz(v, u);
-    return szt[u];
+	szt[u] = 1;
+	for (int v : adj[u]) if (v != p && !del[v])
+		szt[u] += calcsz(v, u);
+	return szt[u];
 }
 void cdfs(int u = 0, int p = -1, int szu = -1) {
-    if (szu < 0) szu = calcsz(u, -1);
-    for (int v : adj[u]) {
-        if (!del[v] && szt[v] * 2 >= szu) {
-            szt[u] = 0;
-            cdfs(v, p, szu);
-            return;
-        }
-    }
-    del[u] = true;
-    par[u] = p;
-    // process centroid u
-    for (int v : adj[u]) if (!del[v]) cdfs(v, u);
+	if (szu < 0) szu = calcsz(u, -1);
+	for (int v : adj[u]) {
+		if (!del[v] && szt[v] * 2 >= szu) {
+			szt[u] = 0;
+			cdfs(v, p, szu);
+			return;
+		}
+	}
+	del[u] = true;
+	par[u] = p;
+	// process centroid u
+	for (int v : adj[u]) if (!del[v]) cdfs(v, u);
 }
 void centroid() {
 	memset(del, false, sizeof(del));
