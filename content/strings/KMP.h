b2536b25e5728346bfef200cfea24ce2756954dd
joaquin30 <46060104+joaquin30@users.noreply.github.com>
2025-11-02 14:31:29 -0500

    añadido kmp automaton

--- content/strings/KMP.h
+++ content/strings/KMP.h
@@ -4,7 +4,7 @@
  * License: CC0
  * Description: pi[x] computes the length of the longest prefix of s that ends at x,
  * other than s[0...x] itself (abacaba -> 0010123).
- * Can be used to find all occurrences of a string.
+ * Can be used to find all occurrences of a string or to build an automaton for a single pattern.
  * Time: O(n)
  * Status: Tested on kattis:stringmatching
  */
@@ -25,4 +25,20 @@ vi match(const string& s, const string& pat) {
 	rep(i,sz(p)-sz(s),sz(p))
 		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
 	return res;
+}
+
+vector<vi> kmp(const string& pat, int alpha = 26) {
+	int m = sz(pat);
+	vector<vi> kmp(m+1, vi(alpha));
+	vi p = pi(pat);
+	rep(i,0,m+1) {
+		rep(c,0,alpha) {
+			int j = i;
+			while (j > 0 && (j == m || pat[j] != c+'a'))
+				j = p[j-1];
+			if (j < m && pat[j] == c+'a') ++j;
+			kmp[i][c]=j;
+		}
+	}
+	return kmp;
 }
